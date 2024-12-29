ffdabc79640eb697e11e5a7e9da02d82086b29f5
Chaim Halbert <chaim.leib.halbert@gmail.com>
2024-12-29 05:33:06 -0700

    add sawtooth, triangle and sine waves

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"math"
@@ -18,7 +19,7 @@ const (
 	chunkSize          = sampleRate * channels * byteDepth / 100
 	duration           = 500 * time.Millisecond
 	frequency  float64 = 440.0
-	amplitude  float64 = 0.05
+	amplitude  float64 = 0.1
 	phase      float64 = 0
 )
 
@@ -34,7 +35,22 @@ func beepTest() error {
 		return err
 	}
 
-	sound, err := tone(duration, frequency, amplitude, phase)
+	squareSound, err := square(duration, frequency, amplitude, phase)
+	if err != nil {
+		return err
+	}
+
+	sawtoothSound, err := sawtooth(duration, frequency, amplitude, phase)
+	if err != nil {
+		return err
+	}
+
+	triangleSound, err := triangle(duration, frequency, amplitude, phase)
+	if err != nil {
+		return err
+	}
+
+	sineSound, err := sine(duration, frequency, amplitude, phase)
 	if err != nil {
 		return err
 	}
@@ -45,30 +61,60 @@ func beepTest() error {
 	}
 
 	// Chunks only play after completely sent to the player, so finish the chunk.
-	chunkFinishLen := (-11 * sound.Len()) % chunkSize
+	chunkFinishLen := (-15 * squareSound.Len()) % chunkSize
 	chunkFinish := make([]byte, chunkFinishLen)
 
 	reader := io.MultiReader(
-		bytes.NewReader(sound.Bytes()),
+		&printReader{name: "square", buf: squareSound.Bytes()},
 		bytes.NewReader(silence.Bytes()),
 
-		bytes.NewReader(sound.Bytes()),
+		&printReader{name: "sawtooth", buf: sawtoothSound.Bytes()},
 		bytes.NewReader(silence.Bytes()),
 
-		bytes.NewReader(sound.Bytes()),
+		&printReader{name: "triangle", buf: triangleSound.Bytes()},
 		bytes.NewReader(silence.Bytes()),
 
-		bytes.NewReader(sound.Bytes()),
+		&printReader{name: "sine", buf: sineSound.Bytes()},
 		bytes.NewReader(silence.Bytes()),
 
-		bytes.NewReader(sound.Bytes()),
+		&printReader{name: "square", buf: squareSound.Bytes()},
 		bytes.NewReader(silence.Bytes()),
 
-		bytes.NewReader(sound.Bytes()),
+		&printReader{name: "sawtooth", buf: sawtoothSound.Bytes()},
+		bytes.NewReader(silence.Bytes()),
+
+		&printReader{name: "triangle", buf: triangleSound.Bytes()},
+		bytes.NewReader(silence.Bytes()),
+
+		&printReader{name: "sine", buf: sineSound.Bytes()},
 		bytes.NewReader(chunkFinish),
 	)
 	_, err = io.Copy(p, reader)
 	return err
+}
+
+type printReader struct {
+	name string
+	buf  []byte
+	i    int
+}
+
+var _ io.Reader = (*printReader)(nil)
+
+func (p *printReader) Read(out []byte) (n int, err error) {
+	if p.i == 0 {
+		fmt.Println(p.name)
+	}
+	end := p.i + len(out)
+	if end > len(p.buf) {
+		end = len(p.buf)
+	}
+	n = copy(out, p.buf[p.i:end])
+	p.i += n
+	if p.i >= len(p.buf) {
+		return n, io.EOF
+	}
+	return n, nil
 }
 
 func monoPlayer() (*oto.Player, error) {
@@ -79,7 +125,7 @@ func monoPlayer() (*oto.Player, error) {
 	return c.NewPlayer(), nil
 }
 
-func tone(duration time.Duration, frequency, amplitude, phase float64) (*samplebuffer.Buffer, error) {
+func square(duration time.Duration, frequency, amplitude, phase float64) (*samplebuffer.Buffer, error) {
 	buf, err := samplebuffer.New(sampleRate, byteDepth, channels, duration)
 	if err != nil {
 		return nil, err
@@ -100,6 +146,76 @@ func tone(duration time.Duration, frequency, amplitude, phase float64) (*sampleb
 		} else {
 			buf.WriteChanSample(-iAmplitude)
 		}
+	}
+
+	return buf, nil
+}
+
+func sawtooth(duration time.Duration, frequency, amplitude, phase float64) (*samplebuffer.Buffer, error) {
+	buf, err := samplebuffer.New(sampleRate, byteDepth, channels, duration)
+	if err != nil {
+		return nil, err
+	}
+
+	// theta0 is initial phase offset in samples
+	periodSamples := float64(sampleRate) / frequency
+	theta0 := phase * periodSamples / (2 * math.Pi)
+	maxAmplitude := buf.MaxValue()
+	iAmplitude := float64(maxAmplitude) * amplitude
+
+	for i := 0; i < buf.SamplesForDuration(duration); i++ {
+		// waveFraction is how far we have progressed into the waveform's repeating
+		// period. It ranges from -0.5 to 0.5.
+		waveFraction := math.Remainder(float64(i)+theta0, periodSamples) / periodSamples
+		buf.WriteChanSample(int(2 * iAmplitude * waveFraction))
+	}
+
+	return buf, nil
+}
+
+func triangle(duration time.Duration, frequency, amplitude, phase float64) (*samplebuffer.Buffer, error) {
+	buf, err := samplebuffer.New(sampleRate, byteDepth, channels, duration)
+	if err != nil {
+		return nil, err
+	}
+
+	// theta0 is initial phase offset in samples
+	periodSamples := float64(sampleRate) / frequency
+	theta0 := (phase + math.Pi/2) * periodSamples / (2 * math.Pi)
+	maxAmplitude := buf.MaxValue()
+	iAmplitude := float64(maxAmplitude) * amplitude
+
+	for i := 0; i < buf.SamplesForDuration(duration); i++ {
+		// waveFraction is how far we have progressed into the waveform's repeating
+		// period. It ranges from -0.5 to 0.5.
+		waveFraction := math.Remainder(float64(i)+theta0, periodSamples) / periodSamples
+		// output varies linearly from -iAmplitude at 0 to iAmplitude at pi, and
+		// back down to -iAmplitude at 2pi. Note that this means output is not 0 at
+		// phase 0, so theta0 includes an offset to provide for this.
+		if waveFraction >= 0.0 {
+			buf.WriteChanSample(int((4*waveFraction - 1.0) * iAmplitude))
+		} else {
+			buf.WriteChanSample(int((-4*waveFraction - 1.0) * iAmplitude))
+		}
+	}
+
+	return buf, nil
+}
+
+func sine(duration time.Duration, frequency, amplitude, phase float64) (*samplebuffer.Buffer, error) {
+	buf, err := samplebuffer.New(sampleRate, byteDepth, channels, duration)
+	if err != nil {
+		return nil, err
+	}
+
+	// theta0 is initial phase offset in samples
+	periodSamples := float64(sampleRate) / frequency
+	maxAmplitude := buf.MaxValue()
+	iAmplitude := float64(maxAmplitude) * amplitude
+
+	for i := 0; i < buf.SamplesForDuration(duration); i++ {
+		theta := float64(i) * 2 * math.Pi / periodSamples
+		buf.WriteChanSample(int(iAmplitude * math.Sin(theta+phase)))
 	}
 
 	return buf, nil
